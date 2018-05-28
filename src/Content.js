c4d6dfe3dd4a50a69db1676d5e1d095c31713d0c
Robert Dayton <robertcdayton@gmail.com>
2018-05-28 17:08:26 -0400

    Certification image fixed, alt descriptions added

--- src/Content.js
+++ src/Content.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './Content.css';
 import aws from "./AWS_Certified_Logo_SAA_588x300_White.png";
 import saa from "./AWS_Certified_Tag__SAA_588x300-White.png";
-import sysops from "./AWS_Certified_Tag__SOA_588x300-White.png";
-import dev from "./AWS_Certified_Tag__SAA_588x300-White.png"
+import sysops from "./AWS_Certified_Tag__SOA_1176x600-White.png";
+import dev from "./AWS_Certified_Tag__DVA_1176x600-White.png"
 
 function highlightSyntax(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g, function (match) {
         var cls = 'number';
         if (/^"/.test(match)) {
             if (/:$/.test(match)) {
@@ -95,16 +95,16 @@ class Content extends Component {
                     </div>
                 }   
                 {this.props.view === "aws" &&
-                    <div class="certifications">
-                        <img src={aws} />
-                        <a href="https://aw.certmetrics.com/amazon/public/verification.aspx?code=B6MP74G2BNQQQBSC" target="_blank"><img src={saa} /></a>
-                        <a href="https://aw.certmetrics.com/amazon/public/verification.aspx?code=8S4FJWWKJ2VE1RK8" target="_blank"><img src={dev} /></a>
-                        <a href="https://aw.certmetrics.com/amazon/public/verification.aspx?code=5B78NBECLFE11MGZ" target="_blank"><img src={sysops} /></a>
+                    <div className="certifications">
+                        <img className="certification-banner" src={aws} alt="AWS Certified" />
+                        <a href="https://aw.certmetrics.com/amazon/public/verification.aspx?code=B6MP74G2BNQQQBSC" target="_blank" rel="noopener noreferrer" ><img className="certification-image" src={saa} alt="AWS Solutions Architect Associate"/></a>
+                        <a href="https://aw.certmetrics.com/amazon/public/verification.aspx?code=8S4FJWWKJ2VE1RK8" target="_blank" rel="noopener noreferrer" ><img className="certification-image" src={dev} alt="AWS Developer Associate" /></a>
+                        <a href="https://aw.certmetrics.com/amazon/public/verification.aspx?code=5B78NBECLFE11MGZ" target="_blank" rel="noopener noreferrer"><img className="certification-image" src={sysops} alt="AWS SysOps Associate" /></a>
                         <p className="announcement">Pro level certifications coming soon.</p>
                     </div>
                 }  
                 {this.props.view === "github" &&
-                    <p className="comment">// TODO: add a github link</p>
+                    <p className="announcement">Github repositories available at <a href="https://github.com/rdayton" target="_blank" rel="noopener noreferrer">https://github.com/rdayton</a></p>
                 }  
                 {this.props.view === "bugs" &&
                     <p className="comment">// TODO: add some weird bug stuff</p>
